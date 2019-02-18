f71077e4a8f9832db0f1528ad3e6546af6bc942f
jufebrown <jufebrown@gmail.com>
2019-02-18 16:14:56 -0600

    update webpack.config.js with babel-loader rules

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
         test: /\.css$/,
         use: [
           'style-loader',
@@ -19,15 +29,15 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
-          'file-loader'
-        ]
+          'file-loader',
+        ],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
-          'file-loader'
-        ]
-      }
+          'file-loader',
+        ],
+      },
     ],
   },
 };
