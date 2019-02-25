c61c08034ae837c31b0462281c6ec05b9c69f575
jufebrown <jufebrown@gmail.com>
2019-02-25 13:50:58 -0600

    change text color to white:

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -1,5 +1,5 @@
 export default class TitleScene extends Phaser.Scene {
   create() {
-    this.add.text(100, 100, 'Hello World', { font: '65px arial', fill: '#0f0' });
+    this.add.text(100, 100, 'Hello World', { font: '65px arial', fill: '#ffff' });
   }
 }
