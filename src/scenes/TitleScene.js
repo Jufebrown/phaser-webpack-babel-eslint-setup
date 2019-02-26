d70192325973cb7811dbb05f635e22e814dc975a
jufebrown <jufebrown@gmail.com>
2019-02-26 11:18:41 -0600

    add startGame method to TitleScene class

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -8,16 +8,16 @@ export default class TitleScene extends Phaser.Scene {
   create() {
     this.add.text(100, 100, 'Hello World', { font: '65px arial', fill: '#b50015' });
 
-    const startGameButton = this.add.text(100, 150, 'Start Game', { font: '14px arial', fill: '#fff' })
+    const startGameButton = this.add.text(100, 200, 'Start Game', { font: '14px arial', fill: '#fff' })
       .setInteractive()
       .on('pointerdown', () => this.startGame())
       .on('pointerover', () => this.enterButtonHoverState())
       .on('pointerout', () => this.enterButtonRestState());
   }
 
-  // startGame() {
-
-  // }
+  startGame() {
+    this.scene.start('GameScene');
+  }
 
   enterButtonHoverState() {
     this.startGameButton.setStyle({ fill: '#b50015' });
