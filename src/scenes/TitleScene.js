8186393f6eb71504be7f1d70e77033da6548475d
jufebrown <jufebrown@gmail.com>
2019-02-26 11:02:27 -0600

    add pointerover and pointerout listeners to startGameButton

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -2,12 +2,22 @@ export default class TitleScene extends Phaser.Scene {
   create() {
     this.add.text(100, 100, 'Hello World', { font: '65px arial', fill: '#b50015' });
 
-    const startGameButton = this.add.text(100, 150, 'Start Game', { font: '14px arial' })
+    const startGameButton = this.add.text(100, 150, 'Start Game', { font: '14px arial', fill: '#fff' })
       .setInteractive()
-      .on('pointerdown', () => this.startGame());
+      .on('pointerdown', () => this.startGame())
+      .on('pointerover', () => this.enterButtonHoverState())
+      .on('pointerout', () => this.enterButtonRestState());
   }
 
   // startGame() {
 
   // }
+
+  enterButtonHoverState() {
+    this.startGameButton.setStyle({ fill: '#b50015' });
+  }
+
+  enterButtonRestState() {
+    this.startGameButton.setStyle({ fill: '#fff' });
+  }
 }
