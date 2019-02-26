9d2acff9300b9a8f6ba171b8e0fb90cfa534b1d2
jufebrown <jufebrown@gmail.com>
2019-02-26 11:31:14 -0600

    add space in GameScene text

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -6,6 +6,6 @@ export default class GameScene extends Phaser.Scene {
   }
 
   create() {
-    this.add.text(100, 100, 'GameScene', { font: '65px arial', fill: '#fff' });
+    this.add.text(100, 100, 'Game Scene', { font: '65px arial', fill: '#fff' });
   }
 }
