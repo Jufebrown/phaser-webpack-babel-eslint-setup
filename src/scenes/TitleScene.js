4281634b8f6c21e94f2e90cfb1c8e73ee1384dd7
jufebrown <jufebrown@gmail.com>
2019-02-26 10:34:34 -0600

    add text for start game button to TitleScene.js

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -1,5 +1,7 @@
 export default class TitleScene extends Phaser.Scene {
   create() {
     this.add.text(100, 100, 'Hello World', { font: '65px arial', fill: '#b50015' });
+
+    this.add.text(100, 150, 'Start Game', { font: '14px arial' });
   }
 }
