6763e9a4c8ae3c45ad6c8ecbd806bdbd2b49be8f
jufebrown <jufebrown@gmail.com>
2019-02-26 10:39:11 -0600

    add click listener to startGameButton

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -2,6 +2,12 @@ export default class TitleScene extends Phaser.Scene {
   create() {
     this.add.text(100, 100, 'Hello World', { font: '65px arial', fill: '#b50015' });
 
-    this.add.text(100, 150, 'Start Game', { font: '14px arial' });
+    const startGameButton = this.add.text(100, 150, 'Start Game', { font: '14px arial' })
+      .setInteractive()
+      .on('pointerdown', () => this.startGame());
   }
+
+  // startGame() {
+
+  // }
 }
