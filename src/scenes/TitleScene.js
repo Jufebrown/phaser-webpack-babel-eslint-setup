eab33b32244846d0f49fef77762a7ca15b415788
jufebrown <jufebrown@gmail.com>
2019-02-26 11:15:02 -0600

    add constructor to TitleScene class

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -1,4 +1,10 @@
 export default class TitleScene extends Phaser.Scene {
+  constructor() {
+    super({
+      key: 'TitleScene',
+    });
+  }
+
   create() {
     this.add.text(100, 100, 'Hello World', { font: '65px arial', fill: '#b50015' });
 
